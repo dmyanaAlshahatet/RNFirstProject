20619209e2313bd66f79bd5687c8972f87f2bb32
dmyana <dmyana2003@gmail.com>
2023-05-24 20:06:46 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,69 @@
+
+import react from "react"; //always do import//
+import { ImageBackground, StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import React, { Component } from 'react'
+import Card from "./src/components/Card";
+import SecCard from "./src/components/SecCard";
+
+const App = () => { //function component//
+
+  const data = [
+    {
+      name: 'brahem',
+      bd: '19.3', 
+      bgc:'red'
+    },
+    {
+      name: 'saleh',
+      bd: '23.6',
+      bgc:'black'
+    },
+    {
+      name: 'huda',
+      bd: '21.10',
+      bgc:'pink'
+    },
+    {
+      name: 'noor',
+      bd: '28.8',
+      bgc:'purple'
+    } , 
+    {
+      name:'zeen' , 
+      bd:'24.8' , 
+      bgc:'blue',
+    }, 
+    {
+      name:'rabea' , 
+      bd:'6.7'
+    }
+  ]
+
+  const renderCards = ()=> {
+    return data.map(cardData => {
+      return <Card name={cardData.name} bd={cardData.bd} bgc={cardData.bgc} />
+    })
+  }
+
+
+
+  return (
+    <View style={styles.container} >
+      <ScrollView showsVerticalScrollIndicator={false}>
+          {renderCards()}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    // justifyContent: 'center',
+    // alignItems: 'center' , 
+    paddingTop: 50
+  },
+
+})
+export default App;
---
+++ src/components/SecCard.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SecCard = () => {
+  return (
+    <View style={styles.card }>
+
+    </View>
+            
+  )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        borderWidth: 1,
+        width: '70%',
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+        alignSelf: 'center',
+    },
+})
+
+
+export default SecCard;
