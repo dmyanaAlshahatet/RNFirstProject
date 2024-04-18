42f89b3c5c085aa4c99143ce8fa94edb779c21c4
dmyana <dmyana2003@gmail.com>
2024-04-18 18:32:25 +0300

    nav changes

--- route/nav.js
+++ route/nav.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { NavigationContainer, DarkTheme, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useColorScheme, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Text } from "react-native";
+import { useColorScheme, View, StyleSheet, Image } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 import Home from "../src/components/screens/Home";
 import ProfileView from "../src/components/ProfileView";
 import TasksList from "../src/components/TasksList";
@@ -17,11 +18,10 @@ import InstagramHome from "../src/components/InstagramHome";
 import AnimTap2 from "./AnimTab2";
 
 const MainNavigation = (props) => {
-
-    const Stack = createNativeStackNavigator()
-
+    const Stack = createNativeStackNavigator();
     const colorScheme = useColorScheme();
     const [theme, setTheme] = useState(colorScheme);
+    const [selectedOption, setSelectedOption] = useState('auto'); // State to track user's choice
 
     const toggleTheme = () => {
         const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -39,11 +39,34 @@ const MainNavigation = (props) => {
                 <Stack.Screen name={ScreenNames.SearchUsers} component={SearchUsers} options={{ headerShown: false }} />
                 <Stack.Screen name={ScreenNames.TasksToDo} component={TasksList} options={{ headerShown: false }} />
                 <Stack.Screen name={ScreenNames.BottomTab} component={BottomTab} options={{ headerShown: false }} />
-                {/* <Stack.Screen name={ScreenNames.AnimTap2} component={AnimTap2} options={{ headerShown: false }} /> */}
             </Stack.Navigator>
-            <TouchableOpacity style={styles.iconContainer} onPress={toggleTheme}>
-                <Image source={theme === 'light' ? require('../src/components/images/sun.png') : require('../src/components/images/moon.png')} style={styles.iconImage} />
-            </TouchableOpacity>
+            <View style={styles.dropdownContainer}>
+                <DropDownPicker
+                    items={[
+                        { label: 'Auto', value: 'auto' },
+                        { label: 'Light', value: 'light' },
+                        { label: 'Dark', value: 'dark' },
+                    ]}
+                    defaultValue={selectedOption}
+                    containerStyle={{ height: 40 }}
+                    style={{ backgroundColor: '#fafafa' }}
+                    itemStyle={{
+                        justifyContent: 'flex-start'
+                    }}
+                    dropDownStyle={{ backgroundColor: '#fafafa' }}
+                    onChangeItem={(item) => setSelectedOption(item.value)} // Update selected option
+                    labelStyle={{ fontSize: 18 }}
+                    arrowIconStyle={styles.arrowIconContainer}
+                    arrowIconContainerStyle={styles.arrowIconContainer}
+                    arrowIconImageStyle={styles.arrowIconImage}
+                    arrowIcon={() => (
+                        <Image
+                            source={selectedOption === 'light' ? require('../src/components/images/sun.png') : require('../src/components/images/moon.png')} // Assuming you have sun.png and moon.png in your project directory
+                            style={styles.arrowIconImage}
+                        />
+                    )}
+                />
+            </View>
         </NavigationContainer>
     )
 }
@@ -51,14 +74,20 @@ const MainNavigation = (props) => {
 export default MainNavigation;
 
 const styles = StyleSheet.create({
-    iconContainer: {
+    dropdownContainer: {
         position: 'absolute',
         top: 20,
-        right: 20,
+        left: 20,
+        zIndex: 1, // Ensure dropdown is above other components
     },
-    iconImage: {
-        width: 26,
-        height: 26,
-
+    arrowIconContainer: {
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    arrowIconImage: {
+        width: 24,
+        height: 24,
     },
 });
