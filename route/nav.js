0fda261eb2cf6bf3bdfe3a4b23e949a5a84e16c6
dmyana <dmyana2003@gmail.com>
2024-04-18 16:43:44 +0300

    nav theme another way

--- route/nav.js
+++ route/nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavigationContainer, DarkTheme, DefaultTheme, } from "@react-navigation/native";
+import { NavigationContainer, DarkTheme, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useColorScheme } from 'react-native';
 import { Text, View } from "react-native";
@@ -14,11 +14,7 @@ import SearchBar1 from "../src/components/SearchBar1";
 import Splash from "../src/components/screens/Splash";
 import SwipeBox from "../src/components/SwipeBox";
 import InstagramHome from "../src/components/InstagramHome";
-import AnimTap2 from "./AnimTab2";
-// import { ThemeProvider } from '../src/context/ThemeContext';
-import Theme from "../src/components/Theme";
-
-
+// import AnimTap2 from "./AnimTab2";
 
 const MainNavigation = (props) => {
 
@@ -26,40 +22,20 @@ const MainNavigation = (props) => {
 
     const colorScheme = useColorScheme();
 
-    // const MyTheme = {
-    //     ...DefaultTheme,
-    //     colors: {
-    //         ...DefaultTheme.colors,
-    //         primary: 'rgb(255, 45, 85)',
-    //     },
-    // };
-
-    // const MyTheme = {
-    //     dark: false,
-    //     colors: {
-    //         primary: 'rgb(255, 45, 85)',
-    //         background: 'rgb(242, 242, 242)',
-    //         card: 'rgb(255, 255, 255)',
-    //         text: 'rgb(28, 28, 30)',
-    //         border: 'rgb(199, 199, 204)',
-    //         notification: 'rgb(255, 69, 58)',
-    //     },
-    // };
+    const MyTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
 
     return (
-        // <ThemeProvider>
-            <NavigationContainer >
-                <Stack.Navigator initialRouteName="Login">
-                    <Stack.Screen name={ScreenNames.Splash} component={Splash} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.InstagramHome} component={InstagramHome} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.Home} component={Home} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.SearchUsers} component={SearchUsers} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.TasksToDo} component={TasksList} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.BottomTab} component={BottomTab} options={{ headerShown: false }} />
-                    <Stack.Screen name={ScreenNames.AnimTap2} component={AnimTap2} options={{ headerShown: false }} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        // </ThemeProvider>
+        <NavigationContainer theme={MyTheme}>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name={ScreenNames.Splash} component={Splash} options={{ headerShown: false }} />
+                <Stack.Screen name={ScreenNames.InstagramHome} component={InstagramHome} options={{ headerShown: false }} />
+                <Stack.Screen name={ScreenNames.Home} component={Home} options={{ headerShown: false }} />
+                <Stack.Screen name={ScreenNames.SearchUsers} component={SearchUsers} options={{ headerShown: false }} />
+                <Stack.Screen name={ScreenNames.TasksToDo} component={TasksList} options={{ headerShown: false }} />
+                <Stack.Screen name={ScreenNames.BottomTab} component={BottomTab} options={{ headerShown: false }} />
+                {/* <Stack.Screen name={ScreenNames.AnimTap2} component={AnimTap2} options={{ headerShown: false }} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
