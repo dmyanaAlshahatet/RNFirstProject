3b159d5585b10baf1297d2ba67991b90d2bf6c32
dmyana <dmyana2003@gmail.com>
2024-05-08 18:41:36 +0300

    add users to Search Data

--- src/components/SearchContent.js
+++ src/components/SearchContent.js
@@ -120,5 +120,4 @@ const SearchContent = props => {
 };
 
 
-
 export default SearchContent;
--- src/components/searchData.js
+++ src/components/searchData.js
@@ -1,35 +1,45 @@
 const searchData = [
-    {
-      id: 0,
-      images: [
-        require('../components/images/post1.jpg'),
-        require('../components/images/profile2.jpg'),
-        require('../components/images/post7.jpg'),
-        require('../components/images/post8.jpg'),
-        require('../components/images/post9.jpg'),
-        require('../components/images/post10.jpg'),
-      ],
-    },
-    {
-      id: 1,
-      images: [
-        require('../components/images/post11.jpg'),
-        require('../components/images/post10.jpg'),
-        require('../components/images/post12.jpg'),
-        require('../components/images/post13.jpg'),
-        require('../components/images/post14.jpg'),
-        require('../components/images/redBoss.jpg'),
-      ],
-    },
-    {
-      id: 2,
-      images: [
-        require('../components/images/post1.jpg'),
-        require('../components/images/post1.jpg'),
-        require('../components/images/profile4.jpg'),
-      ],
-    },
-  ];
-  
-  export default searchData;
-  
+  {
+    id: 0,
+    images: [
+      require('../components/images/post1.jpg'),
+      require('../components/images/profile2.jpg'),
+      require('../components/images/post7.jpg'),
+      require('../components/images/post8.jpg'),
+      require('../components/images/post9.jpg'),
+      require('../components/images/post10.jpg'),
+    ],
+  },
+  {
+    id: 1,
+    images: [
+      require('../components/images/post11.jpg'),
+      require('../components/images/post10.jpg'),
+      require('../components/images/post12.jpg'),
+      require('../components/images/post13.jpg'),
+      require('../components/images/post14.jpg'),
+      require('../components/images/redBoss.jpg'),
+    ],
+  },
+  {
+    id: 2,
+    images: [
+      require('../components/images/post1.jpg'),
+      require('../components/images/post1.jpg'),
+      require('../components/images/profile4.jpg'),
+    ],
+  },
+  {
+    type: 'images',
+    images: [require('../components/images/redBoss.jpg'), require('../components/images/redBoss.jpg'), require('../components/images/redBoss.jpg')],
+  },
+  {
+    type: 'users',
+    users: [
+      { name: 'chillhouse', profilePic: require('../components/images/redBoss.jpg') },
+      { name: 'The_Groot', profilePic: require('../components/images/redBoss.jpg') },
+    ],
+  },
+];
+
+export default searchData;
